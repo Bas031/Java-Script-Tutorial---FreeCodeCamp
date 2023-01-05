6bf706bc06fabb04e7413d67d66bb6a12a39661c
Bas031 <67750434+Bas031@users.noreply.github.com>
2023-01-05 16:38:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java-Script-Tutorial---FreeCodeCamp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ktdcx9)
---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("hello from Basil Green")
+var number = 5; in-line comment 
+
+/* this is a 
+i 
+can 
+put anything 
+in-between
+here
+multiline comment */
+number = 9; 
+
+
+/* Data types: 
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Basil"
+
+myName = 7
+
+let ourName = "Lechem Motivations (Pty) Ltd"
+
+const pi = 3.14
+
+var a; 
+var b = 2; 
+console.log(a)
+a = 7; 
+
+b = a; 
+
+console.log(a)
+
+var a = 9; 
+
+//initialise these variables
+var a = 5;
+var b = 10;
+var c "I am a";
+
+// do not code below this line 
+
+a = a + 1; 
+b = b + 5; 
+c = c + "String";
+
